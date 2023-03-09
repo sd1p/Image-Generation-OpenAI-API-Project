fd200ec2d44c798d3f811427b35eaba0cce9a44d
Sudip Mandal <skmxyt@gmail.com>
2023-03-09 15:19:50 +0530

    Updated CSS

--- controller/openaiController.js
+++ controller/openaiController.js
@@ -5,18 +5,24 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
+
+
 const generateImage= async(req,res)=>{
 
     const {prompt, size}= req.body;
     const imageSize = size === 'small'?'256x256': 'medium'? '512x512' : '1024x1024';
+
     try {
         const response= await openai.createImage({
             prompt: prompt,
             n:1,
             size: imageSize
         });
+
         const imageUrl= response.data.data[0].url;
+
         console.log(response.data.data[0].url);
+        
         res.status(200).json({
             success:true,
             imageUrl
